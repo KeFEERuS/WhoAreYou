87552a0d490a5a9cea855a544e9753ea1c05308c
Red Tritium <feer43@gmail.com>
2019-10-20 14:02:20 +0300

    Шатал всех шатал!

---
+++ app/src/main/java/com/example/whoareyou/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.whoareyou;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button exit = (Button) findViewById(R.id.btn_exit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+    }
+}
